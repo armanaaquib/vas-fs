d2f37ee07fc22c5a0e8e86f12dd3809b306b7e02
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-07-05 09:42:40 +0530

    Added: Content-Type

--- fs.js
+++ fs.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const getFid = require('./lib/getFid');
 
+const types = {};
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -16,17 +18,21 @@ app.post('/save', (req, res) => {
   req.on('end', () => {
     const buffer = Buffer.concat(data);
 
+    const contentType = req.headers['content-type'];
     const fid = getFid();
+    types[fid] = contentType;
+
     fs.writeFile(`files/${fid}`, buffer, () => {
       res.end(fid);
     });
   });
 });
 
-app.get('/:fid', (req, res) => {
+app.get('get/:fid', (req, res) => {
   const fid = req.params.fid;
 
   fs.readFile(`files/${fid}`, (err, buffer) => {
+    res.setHeader('Content-Type', types[fid]);
     res.end(buffer);
   });
 });
